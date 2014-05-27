a4b80a5fdb50bd6748b7ffb639666519f886acfe
reds <dominique.jollien@heig-vd.ch>
2014-05-27 17:53:19 +0200

    update place

--- ApplicationRPG/src/game/Game.java
+++ ApplicationRPG/src/game/Game.java
@@ -7,7 +7,7 @@ import gameContent.Place;
 
 public class Game {
 	
-	Place[] places;
+	//Place[] places;
 	
 	public Game() {
 		try {
@@ -17,25 +17,25 @@ public class Game {
 		}
 		UserInterface ui = UserInterface.getInstance();
 		
-		places = new Place[10];
+		//places = new Place[10];
 		
-		Place shore = new Place("rivage", "Vous êtes un jeune aventurier ayant fui la justice de votre contrée natale.\nVous voguez vers le pays d’Orghal en traversant la mer d’Embarh.\nAlors que votre frêle esquif se rapproche de la côte vous mettez en panne, observant votre objectif.\nVous en savez peu sur ce contient. On raconte qu’il est peuplé de créatures étranges et comporte des régions aux environnements très variés.\nOn dit aussi que la vie y est rude. Les habitants y sont sans cesse sous la menace d’une nouvelle guerre entre seigneurs et certains d’entre eux seraient des plus sauvages.\nUn lieu de tous les dangers certes, mais aux grandes richesses pour celui qui est assez téméraire pour les braver. Et de toute façon, il est hors de question de retourner en arrière.\nDans la précipitation du départ vous n’avez guère emporté d’équipement. Des vêtements estivaux, une escarcelle peu remplie, un couteau et une canne à pêche de fortune pour le voyage.\nCela fait des semaines que vous êtes en mer. La faim vous tiraille. Vous êtes impatient d’accoster.\nHeureusement, vous êtes plus ou moins parvenus à tenir votre cap et êtes arrivé à l’endroit prévu.\nVotre carte vendue par des contrebandiers signale une crique proche que vous repérer au bout d’un moment avant d’y amarrer votre bateau.");
-		shore.addPossibleAction("pecher");
+		//Place shore = new Place("rivage", "Vous êtes un jeune aventurier ayant fui la justice de votre contrée natale.\nVous voguez vers le pays d’Orghal en traversant la mer d’Embarh.\nAlors que votre frêle esquif se rapproche de la côte vous mettez en panne, observant votre objectif.\nVous en savez peu sur ce contient. On raconte qu’il est peuplé de créatures étranges et comporte des régions aux environnements très variés.\nOn dit aussi que la vie y est rude. Les habitants y sont sans cesse sous la menace d’une nouvelle guerre entre seigneurs et certains d’entre eux seraient des plus sauvages.\nUn lieu de tous les dangers certes, mais aux grandes richesses pour celui qui est assez téméraire pour les braver. Et de toute façon, il est hors de question de retourner en arrière.\nDans la précipitation du départ vous n’avez guère emporté d’équipement. Des vêtements estivaux, une escarcelle peu remplie, un couteau et une canne à pêche de fortune pour le voyage.\nCela fait des semaines que vous êtes en mer. La faim vous tiraille. Vous êtes impatient d’accoster.\nHeureusement, vous êtes plus ou moins parvenus à tenir votre cap et êtes arrivé à l’endroit prévu.\nVotre carte vendue par des contrebandiers signale une crique proche que vous repérer au bout d’un moment avant d’y amarrer votre bateau.");
+		//shore.addPossibleAction("pecher");
 		
-		Place sherwoodForest = new Place("foret", "Vous entrez dans la forêt de Sherwood.\nLes grands arbres assombrissent considérablement le ciel et le sol foisonne de verdure humide et poisseuse à cause de la mer proche.\nLa forêt est traversée par une route reliant des villages côtiers et celui de Foulit. Ça en fait une bonne tanière pour des brigands de grand-chemins.");
-		shore.addConnectedPlace(sherwoodForest);
-		sherwoodForest.addConnectedPlace(shore);
-		sherwoodForest.addPossibleAction("chasser");
+		//Place sherwoodForest = new Place("foret", "Vous entrez dans la forêt de Sherwood.\nLes grands arbres assombrissent considérablement le ciel et le sol foisonne de verdure humide et poisseuse à cause de la mer proche.\nLa forêt est traversée par une route reliant des villages côtiers et celui de Foulit. Ça en fait une bonne tanière pour des brigands de grand-chemins.");
+		//shore.addConnectedPlace(sherwoodForest);
+		//sherwoodForest.addConnectedPlace(shore);
+		//sherwoodForest.addPossibleAction("chasser");
 		
-		Place muytakTower = new Place("tour de Muytak", ""); //TODO : place en tant qu'enum qui contient les différents place prédéfinis ?
+		//Place muytakTower = new Place("tour de Muytak", ""); //TODO : place en tant qu'enum qui contient les différents place prédéfinis ?
 		
-		places[0] = shore;
-		places[1] = sherwoodForest;
-		places[2] = muytakTower;
+		//places[0] = shore;
+		//places[1] = sherwoodForest;
+		//places[2] = muytakTower;
 		
-		Player player = new Player(10, 10, shore);
+		Player player = new Player(10, 10, Place.shore);
 		System.out.println("Bienvenue dans le jeu. \"help\" affiche les commandes.\n");
-		shore.showInfo();
+		Place.shore.showInfo();
 		
 //		goToPlace(player, sherwoodForest);
 //		goToPlace(player, muytakTower);
@@ -62,7 +62,7 @@ public class Game {
 				commandChunks = command.split(" ");
 				switch (commandChunks[0]) {
 				case "go" :
-					Place place = getPlaceByName(commandChunks[1]);
+					Place place = Place.getPlaceByName(commandChunks[1]);
 					if(place == null) {
 						System.out.println("Destination inconnue.");
 					} else {
@@ -72,10 +72,10 @@ public class Game {
 				case "help" :
 					showHelp();
 					break;
+				case "exit" :
+					return;
 				default:
-					if(!place.gererCommande(command)) {
-						System.out.println("commande inconnue");
-					}
+					player.getCurrentPlace().gererCommande(command);
 					break;
 				}
 			}
@@ -85,17 +85,18 @@ public class Game {
 	public void showHelp() {
 		System.out.println("help - afficher l'aide");
 		System.out.println("go x - aller à x");
-		System.out.println("autres commande selon l'endroit disponible");
+		System.out.println("exit - quitter le jeu");
+		System.out.println("autres commandes selon l'endroit disponible");
 	}
 	
-	public Place getPlaceByName(String name) {
-		for (Place aPlace : places) {
-			if(aPlace.name.equals(name)) {
-				return aPlace;
-			}
-		}
-		return null;
-	}
+//	public Place getPlaceByName(String name) {
+//		for (Place aPlace : places) {
+//			if(aPlace.name.equals(name)) {
+//				return aPlace;
+//			}
+//		}
+//		return null;
+//	}
 
 	public static void main(String[] args) {
 		new Game();
--- ApplicationRPG/src/gameContent/Place.java
+++ ApplicationRPG/src/gameContent/Place.java
@@ -3,13 +3,59 @@ package gameContent;
 import java.util.LinkedList;
 
 public abstract class Place {
+	
+	public static Place 
+		shore = new Place("rivage", "Marchant sur la grève vers la terre, vous tâcher de vous repérer.\nD’après la carte, au nord devrait se trouver un village du nom de Foulit. Au nord-ouest, la forêt de Sherwood. Au nord-est, le monastère côtier de Lindisfarne.", "Vous êtes un jeune aventurier ayant fui la justice de votre contrée natale.\nVous voguez vers le pays d’Orghal en traversant la mer d’Embarh.\nAlors que votre frêle esquif se rapproche de la côte vous mettez en panne, observant votre objectif.\nVous en savez peu sur ce contient. On raconte qu’il est peuplé de créatures étranges et comporte des régions aux environnements très variés.\nOn dit aussi que la vie y est rude. Les habitants y sont sans cesse sous la menace d’une nouvelle guerre entre seigneurs et certains d’entre eux seraient des plus sauvages.\nUn lieu de tous les dangers certes, mais aux grandes richesses pour celui qui est assez téméraire pour les braver. Et de toute façon, il est hors de question de retourner en arrière.\nDans la précipitation du départ vous n’avez guère emporté d’équipement. Des vêtements estivaux, une escarcelle peu remplie, un couteau et une canne à pêche de fortune pour le voyage.\nCela fait des semaines que vous êtes en mer. La faim vous tiraille. Vous êtes impatient d’accoster.\nHeureusement, vous êtes plus ou moins parvenus à tenir votre cap et êtes arrivé à l’endroit prévu.\nVotre carte vendue par des contrebandiers signale une crique proche que vous repérer au bout d’un moment avant d’y amarrer votre bateau.") {
+			public void gererCommande(String command) {
+				String[] commandChunks = command.split(" ");
+				for (String action : possibleActions) {
+					if(action.equals(commandChunks[0])) {
+						switch (action) {
+							case "pecher" : System.out.println("vous pechez");
+											return;
+						}
+					}
+				}
+				System.out.println("commande inconnue");
+			}
+		},
+		sherwoodForest = new Place("foret", "Vous vous tenez dans l’herbe sous l’épaisse frondaison.", "Vous entrez dans la forêt de Sherwood.\nLes grands arbres assombrissent considérablement le ciel et le sol foisonne de verdure humide et poisseuse à cause de la mer proche.\nLa forêt est traversée par une route reliant des villages côtiers et celui de Foulit. Ça en fait une bonne tanière pour des brigands de grand-chemins.") {
+			public void gererCommande(String command) {
+				String[] commandChunks = command.split(" ");
+				for (String action : possibleActions) {
+					if(action.equals(commandChunks[0])) {
+						switch (action) {
+							case "chasser" : System.out.println("vous chassez");
+											return;
+						}
+					}
+				}
+				System.out.println("commande inconnue");
+			}
+		},
+		muytakTower = new Place("tour de Muytak", "", "") {
+			public void gererCommande(String command) {
+				System.out.println("commande inconnue");
+			}
+		};
+	private static Place[] places = new Place[]{shore, sherwoodForest, muytakTower};
+	
+	static {
+		shore.addPossibleAction("pecher");
+		shore.addConnectedPlace(sherwoodForest);
+		sherwoodForest.addPossibleAction("chasser");
+		sherwoodForest.addConnectedPlace(shore);
+	}
+	
 	public String name;
 	public String firstVisitText;
+	public String visitText;
 	public boolean visited;
 	public LinkedList<Place> connectedPlaces;
 	public LinkedList<String> possibleActions;
-	public Place(String name, String firstVisitText) {
+	public Place(String name, String visitText, String firstVisitText) {
 		this.name = name;
+		this.visitText = visitText;
 		this.firstVisitText = firstVisitText;
 		visited = false;
 		connectedPlaces = new LinkedList<Place>();
@@ -33,6 +79,7 @@ public abstract class Place {
 			System.out.println(firstVisitText);
 			visited = true;
 		}
+		System.out.println(visitText);
 		System.out.println("Destinations possibles : ");
 		for (Place place : connectedPlaces) {
 			System.out.println(place.name);
@@ -43,5 +90,14 @@ public abstract class Place {
 		}
 	}
 
-	public abstract boolean gererCommande(String command);
+	public static Place getPlaceByName(String name) {
+		for (Place aPlace : places) {
+			if(aPlace.name.equals(name)) {
+				return aPlace;
+			}
+		}
+		return null;
+	}
+	
+	public abstract void gererCommande(String command);
 }
